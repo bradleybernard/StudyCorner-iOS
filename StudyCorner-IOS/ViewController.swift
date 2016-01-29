d4cb23f3a630d62db94a9f2316d829f03d0c562f
Brad Bernard <bradbernard@me.com>
2016-01-29 15:34:01 -0800

    first commit

---
+++ StudyCorner-IOS/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  StudyCorner-IOS
+//
+//  Created by Brad Bernard on 1/29/16.
+//  Copyright © 2016 Brad Bernard. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
